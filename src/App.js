f47a1aaff95c21a729c82b029760303744c23dc4
Greta Solomon <gs2806@nyu.edu>
2019-11-17 16:20:04 +0800

    420

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
 
 function Profile(props) {
 
- const [activate, setActivate] = useState("firstvideo");
+ const [activate, setActivate] = useState("");
 
  function ecoMode (e) {
    setActivate (e.target.id);
@@ -26,25 +26,34 @@ function Profile(props) {
     <div className="profilepage">
       
       <div className="firstline">
-      <div id="vegan"><img id="digitalvegan" onClick src= "https://i.ibb.co/2jcfkzt/7cefbf7e-63b6-4843-8fbe-72afec0f5961-200x200.png"/></div>
+      <button id="empty"></button>
       <button id="learn" onClick>Learn More</button>
       <button id="chrome" onClick>Chrome Extension</button>
     </div>
-
+    <div id="vegan"> <img src= "https://i.ibb.co/2jcfkzt/7cefbf7e-63b6-4843-8fbe-72afec0f5961-200x200.png" height = "150" width = "150" /></div>
       <div className="main">
       
-      <div className= "text"> <p> Keep your digital life clean to keep the earth green with a click of a button </p></div>
+      <div className= "text"> <strong><p> "Keep your digital life clean to keep the earth green with a click of a button" </p></strong></div>
       <div className = 'container'>
       <div id= "video"><iframe width="520" height="315" src={mainContent[0]} frameborder="0" allow="autoplay" mute></iframe> </div>
       {/* <img id="ecopic" src="" width="350" alt="Download Icon Png Leaf" /> */}
-      <iframe id="firstvideo" src={(activate==="firstvideo") ? mainContent[0] : mainContent[3]} height="315" width="520" frameborder="0" allow="autoplay" mute alt="vid" ></iframe> 
+      <div id= "video2"><iframe width="520" height="315" src={mainContent[0]} frameborder="0" allow="autoplay" mute></iframe> </div>
       </div>
-      <div className= "canyou"> <p> Can you tell the difference? </p></div>
+      
+      <div className ="bighd">
+      <div id= "hd"> <strong>HD </strong></div>
+      <div id= "eco"><strong>Eco Mode</strong></div>
+      </div>
+      
       <div className = "clickers">
-      <button id = "text1"> <strong><p> Yes! </p></strong></button>
-      <button id = "text2"> <strong><p1> No! </p1></strong></button>
+      <div id= "canyou"> <p id="line"> Can you tell the difference? </p>
+      <button id = "text1" onClick={ecoMode}> <strong><p> Yes! </p></strong></button>
+      <button id = "text2" onClick={ecoMode}> <strong><p> No! </p></strong></button></div>
+      <p className ="displayco2">{(activate==="text1") ? "The difference is that you are saving 50kg of CO2" : ""} {(activate==="text2") ? "The difference is you are saving 50kg of CO2" : ""}</p>
       </div>
       <img id="iconbutton" onClick={ecoMode} src={mainContent[1]} height="200" width="100" alt="value"/>
+
+
       </div></div>
 
      
