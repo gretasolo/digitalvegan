7090a44fa5ec209f127637483f8d76f8fef61436
Greta Solomon <gs2806@nyu.edu>
2019-11-17 20:34:19 +0800

    834

--- src/App.js
+++ src/App.js
@@ -52,6 +52,18 @@ function Profile(props) {
       <p className ="displayco2">{(activate==="text1") ? "The difference is that you are saving 50kg of CO2" : ""} {(activate==="text2") ? "The difference is you are saving 50kg of CO2" : ""}</p>
       </div>
       <img id="iconbutton" onClick={ecoMode} src={mainContent[1]} height="200" width="100" alt="value"/>
+      <div id= "learnmore"> <p>
+<strong>Learn More</strong> <br></br><br></br>
+
+<u>Problem: </u>
+ 
+The high amounts of data usage by data centers and the resulting carbon emissions in the environment. The unregulated amount of carbon emissions emitted per view of high-res videos and online media.
+
+<br></br><br></br><u>Solution/ Business model:</u>
+
+The creation of an application interface or program add-on that enables the user to conveniently shift between a low-energy consumption mode and the regular viewing setting of high-resolution videos and streaming content.
+
+Giving the users the freedom to choose to limit the amount of loadable content on their webpage through the “Eco-mode add-on”</p></div>
 
 
       </div></div>
